a9b35842c85441e06763ae66b8cf6f35eb1be611
Mitch Allen <mitch.c.allen@gmail.com>
2017-01-19 12:16:01 -0700

    removed mochaTest from Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,26 +107,11 @@ module.exports = function(grunt) {
         }
     },
 
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec',
-          captureFile: 'results.txt', // Optionally capture the reporter output to a file
-          quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
-          noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
-        },
-        src: ['test/**/*.js']
-      }
-    }
-
   });
 
   grunt.registerTask('default', ['build']);
-  grunt.registerTask('pretest', ['clean', 'upcoming', 'upcoming:patch', 'upcoming:major']);
-  grunt.registerTask('test', ['pretest', 'mochaTest']);
   grunt.registerTask('build-doc', ['jsdoc2md']);
-  grunt.registerTask('build', ['jshint', 'test']);
+  grunt.registerTask('build', ['clean','jshint']);
   grunt.registerTask('pubinit', ['build','shell:pubinit']);
   grunt.registerTask('publish', ['build','bump','shell:publish']);
 
